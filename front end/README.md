f987f134cb57f357952058882559c20a2a8a025b
Samuel Cook <slc142@case.edu>
2023-10-03 13:20:44 -0400

    more frontend

---
+++ front end/README.md
@@ -0,0 +1,13 @@
+Folder for the front end of the senior project.
+
+It consists of a python file that will run Flask to host the HTML pages/
+
+There should be 2 main pages
+- a home page that allows for inputs to run the training
+- a results page to show the results of training the model
+
+In order to run the flask application use this command in the terminal:
+	
+`flask --app app run`
+
+Additionally, add `--debug` at the end of the command to run in debug mode so that refreshing the page will load any changes to the code without having to restart the app.
--- front end/app.py
+++ front end/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
-# flask --app app run --debug
+# http://127.0.0.1:5000/results?model=model_name&dataset=mydata&parameters=11
+@app.route("/results", methods=['GET'])
+def results():
+    model = request.args.get('model')
+    dataset = request.args.get('dataset')
+    parameters = request.args.get('parameters')
+    return render_template('results.html', model=model, dataset=dataset, parameters=parameters)
 
-# model name, dataset, parameters
+# flask --app app run --debug
