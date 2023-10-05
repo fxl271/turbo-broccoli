2ed92239fd67654fc686198bd746275c141aa2e8
unknown <LynGarnes@gmail.com>
2023-10-05 12:35:39 -0400

    gaming

--- front end/app.py
+++ front end/app.py
@@ -2,16 +2,29 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET','POST'])
 def homepage():
+    if request.method == 'POST':
+        modelTag = request.form['modelDropdown']
+        #value = tag_lookup(tag)
+        print(modelTag)
+        datasetTag = request.form['datasetDropdown']
+        #value = tag_lookup(tag)
+        print(datasetTag)
+    
     return render_template('homepage.html')
+
+
 
 # http://127.0.0.1:5000/results?model=model_name&dataset=mydata&parameters=11
 @app.route("/results", methods=['GET'])
 def results():
     model = request.args.get('model')
+    
     dataset = request.args.get('dataset')
     parameters = request.args.get('parameters')
     return render_template('results.html', model=model, dataset=dataset, parameters=parameters)
 
-# flask --app app run --debug
+
+
+# flask --app app run --debug   
