e7e5771a6a64d23019058cc0bb55f52e1981cd2b
unknown <LynGarnes@gmail.com>
2023-10-10 13:17:56 -0400

    'dsgcoiu

--- frontEnd/backend.py
+++ frontEnd/backend.py
@@ -3,15 +3,12 @@ from TrainApp import train_model
 
 
 class MLThing:
-    def __train__(self):
-        train_model(self.mlModel, self.dataset, self.params)
-        
     def __init__(self, mlModel, dataset, params):
         self.mlModel = mlModel
         self.dataset = dataset
         self.params = params
         
-        __train__(self.mlModel, self.dataset, self.params)
+        train_model(self.mlModel, self.dataset, self.params)
         
     
     
