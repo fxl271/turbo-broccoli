63d381ab6b59a6b2a36210b47afe7afa8c16060b
Samuel Cook <slc142@case.edu>
2023-10-13 14:48:47 -0400

    put a table on the results page

--- frontEnd/app.py
+++ frontEnd/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+import sys
+from flask import Flask, Response, render_template, request, redirect, stream_with_context, url_for
 from backend import *
 
 app = Flask(__name__)
@@ -15,10 +16,16 @@ def homepage():
         
         mlInstance = MLThing(modelTag, datasetTag, 5)
         print(mlInstance.params)
+        return render_template('results.html', model=modelTag, dataset=datasetTag, parameters=mlInstance.params)
     
     return render_template('homepage.html')
 
-
+# @app.route("/output")
+# def output():
+#     def get_stdout():
+#         for i in range(10):
+#             yield str(i)
+#     return Response(get_stdout(), mimetype="text/event-stream")
 
 # http://127.0.0.1:5000/results?model=model_name&dataset=mydata&parameters=11
 @app.route("/results", methods=['GET'])
