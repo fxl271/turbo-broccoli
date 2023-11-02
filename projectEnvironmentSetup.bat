ab4fb39b232e9eb0eda886caab73aed478693477
Xochitl Villalvazo Izquierdo <79888441+xochitlVillalvazoIzquierdo@users.noreply.github.com>
2023-11-02 12:40:32 -0400

    Combined first four commands to one

--- projectEnvironmentSetup.bat
+++ projectEnvironmentSetup.bat
@@ -2,14 +2,11 @@
 
 echo "Starting project environment setup"
 
-rem These commands need to be run manually unfortunately
-rem Simply copy these and run them on the command line
-module load Python/3.10.8-GCCcore-12.2.0
-export PYTHONUSERBASE=$HOME/.usr/local/python/3.10.8
-mkdir -p $PYTHONUSERBASE
-export PATH=$PATH:$HOME/.usr/local/python/3.10.8/bin
+# These commands need to be run manually unfortunately
+# Simply copy these and run them on the command line
+module load Python/3.10.8-GCCcore-12.2.0 ; export PYTHONUSERBASE=$HOME/.usr/local/python/3.10.8 ; mkdir -p $PYTHONUSERBASE ; export PATH=$PATH:$HOME/.usr/local/python/3.10.8/bin
 
-rem These commands work fine
+# These commands work fine
 python -m pip install --user Flask
 python -m pip install --user pandas
 python -m pip install --user transformers
